1af525c91376b9ec173854d9a11c5c30657ba734
seba2311 <villarroel.paiva.sebastian@gmail.com>
2022-06-22 21:11:56 -0400

    confirmar

--- proyecto/core/views.py
+++ proyecto/core/views.py
@@ -1,19 +1,18 @@
-
 from ast import If
 from dataclasses import dataclass
 from email import message
 from pyexpat.errors import messages
+from django.http import Http404
 from django.shortcuts import redirect, render,get_object_or_404
-
 from .forms import UsuarioForm,ProductoForm,CustomUserCreationForm
-
-
 from .models import Usuario,Producto
 from django.contrib.auth import authenticate,login
 from django.contrib import messages
 from rest_framework import viewsets
 from .serialazers import ProductoSerialaizer
 from django.contrib.auth.decorators import login_required, permission_required
+from django.core.paginator import Paginator
+from django.http import Http404
 # Create your views here.
 def home (request):
     usuario = Usuario.objects.all()
@@ -64,8 +63,17 @@ def tienda2(request):
 @permission_required('core.change_producto')
 def carrito(request):
     producto = Producto.objects.all()
+    page = request.GET.get('page',1)
+
+    try:
+        paginator=Paginator(producto, 3)
+        producto=paginator.page(page)
+    except:
+        raise Http404
+
     datos={
-        'productos':producto
+        'entity':producto,
+        'paginator':paginator
     }
     return render(request,'core/carrito.html',datos)
 
@@ -106,6 +114,7 @@ def eliminar_producto(request, id):
     producto=get_object_or_404(Producto, id_producto=id)
     producto.delete()
     return redirect(to="carrito")
+
 
 def usuarios(request):
     return render(request, 'core/usuarios.html')
